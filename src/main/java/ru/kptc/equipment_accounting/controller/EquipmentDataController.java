353409eba5296fdb2aee7e15d93cb19036b88f79
Konstantin <82874257+Bronnice@users.noreply.github.com>
2024-04-10 21:38:37 +0300

    Переписал на Spring MVC, доделал (#1)

--- src/main/java/ru/kptc/equipment_accounting/controller/EquipmentDataController.java
+++
@@ -1,4 +0,0 @@
-package ru.kptc.equipment_accounting.controller;
-
-public class EquipmentDataController {
-}
--- src/main/java/ru/kptc/equipment_accounting/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.kptc.equipment_accounting.exception;
-
-public class ValidationException extends Exception{
-    public ValidationException(String message) {
-        super(message);
-    }
-}
