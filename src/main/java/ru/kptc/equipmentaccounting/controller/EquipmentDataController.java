39dcec87ad5e99ea2e67ab727a21eec764c503b0
kponomarev <kponamarev@mmtr.ru>
2024-04-10 20:51:49 +0300

    mock

--- src/main/java/ru/kptc/equipmentaccounting/controller/EquipmentDataController.java
+++ src/main/java/ru/kptc/equipmentaccounting/controller/EquipmentDataController.java
@@ -1,14 +1,10 @@
 package ru.kptc.equipmentaccounting.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.kptc.equipmentaccounting.dao.EquipmentDataDao;
 import ru.kptc.equipmentaccounting.dao.SystemBlockDao;
 import ru.kptc.equipmentaccounting.service.EquipmentDataService;
